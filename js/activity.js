fe8f2183297bfdb5428e082b68d63701f1381cd8
Abdurahman Baktybek uulu <abdurahmanbaktybekuulu@Abdurahmans-Air.lan>
2022-10-18 02:51:59 -0700

    dede

--- js/activity.js
+++ js/activity.js
@@ -1,4 +1,20 @@
-$("td").click(function(){
-    $("td.selected").removeClass("selected");
-    $(this).addClass("selected");
-});
+$(document).ready(function() {
+    $("td").click(function() {
+        var content = $(this).text();
+        if(content != "Not Available") {
+            if($(this).hasClass(tdhighlight)) {
+                $('displaySelected').css("visibility", "visible");
+                $('displaySelected').css("margin-top", "2em");
+                $('#result').append("<p>" + content + "</p>");
+
+            }
+         else {
+            $('#result p:contains('+content + ')').remove();
+            if($("#result").has('p').length == false) {
+                $('#displaySelected').css('visibility', 'hidden');
+                $('displaySelected').css("margin-top", "0");
+            }
+            }
+        }
+    })
+})
