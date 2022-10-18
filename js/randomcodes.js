d0a7d10309793acdc72d1ab9da35da45187a9be2
Abdurahman Baktybek uulu <abdurahmanbaktybekuulu@Abdurahmans-Air.lan>
2022-10-18 01:39:05 -0700

    edited the randomcode.html file

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,5 +1,7 @@
 function generateCode() {
     var code  = ' ';
+    var getCode = '';
+    var  btnValue;
     var str = "ABCDEFGHIJKLMONPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789@#$";
     for(i = 1; i <= 8; i++) {
         var char = Math.random() * str.length;
@@ -9,8 +11,25 @@ function generateCode() {
 }
 document.getElementById("code").innerHTML = generateCode();
 
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnValue) {
+    document.getElementById("submit").disabled = btnValue;
+    if(btnValue == true) {
+        document.getElementById("submit").style.background = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)"
+    } else {
+        document.getElementById("submit").style.background = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)"
+    }
 }
+var codebox = document.getElementById("codeentered");
+codebox.addEventListener("input", evaluateCode);
 
-disableButton();
+function evaluateCode() {
+    getCode = document.getElementById('codeentered').value;
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+    if(charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false);
+    }
+}
+documents.getElementById("partners").inneHTML = imageList;
